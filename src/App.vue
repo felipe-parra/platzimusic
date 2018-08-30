588617a2c4d156ef85914bfb90a278f037584148
phick-ws <fpr29.10@gmail.com>
2018-08-29 23:31:31 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,86 @@
+<template lang="pug">
+    #app
+      img(src='./assets/logo.png')
+      h1 PlatziMusic
+      select(v-model="selectedCountry")
+        option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
+      spinner(v-show='loading')
+      ul
+        artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
+
+</template>
+
+<script>
+import Artist from './components/Artist.vue'
+import Spinner from './components/Spinner.vue'
+import getArtists from './api'
+export default {
+  name: 'app',
+  data () {
+    return {
+      artists:[],
+      countries:[
+          { name:'Argentina', value:'argentina'},
+          { name:'Colombia', value:'colombia'},
+          { name:'España', value:'spain'},
+          { name:'Mexico', value:'mexico'},
+          { name:'USA', value:'us'}
+      ],
+      selectedCountry: 'argentina',
+      loading:true
+    }
+  },
+  components: {
+      Artist,
+      Spinner
+  },
+  mounted: function(){
+      this.refreshArtists()
+  },
+  methods:{
+    refreshArtists() {
+      const self = this
+      this.loading = true
+      this.artist = []
+      getArtists(this.selectedCountry)
+        // .then(function (artists) {
+        //
+        //   self.artists = artists
+        // })
+        .then(artists => self.artists = artists)
+        .catch(error => console.error(error))
+        .finally(() => self.loading = false);
+      }
+  },
+  watch: {
+    selectedCountry(){
+      this.refreshArtists()
+      }
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+  margin-top 60px
+
+h1, h2
+  font-weight normal
+
+ul
+  list-style-type none
+  padding 0
+
+li
+  display inline-block
+  margin 0 10px
+
+a
+  color #42b983
+    color #42b983
+</style>
---
+++ src/api/config.js
@@ -0,0 +1,14 @@
+export default {
+    appname: 'platzimusic',
+    apiKey: 'e8bdbb553a0c47cb5845a4ff026ab3b3',
+    secret: 'f38dc6d778f78713e30322e33f7e2b30',
+    registeredTo: 'pipepico'
+}
+
+
+
+
+// Application name	platzimusic
+// API key	e8bdbb553a0c47cb5845a4ff026ab3b3
+// Shared secret	f38dc6d778f78713e30322e33f7e2b30
+// Registered to	pipepico
